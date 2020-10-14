78bcee8bec086343ac1120c214f2ee1c15c8ce29
jeffss99 <jeff@mentorcity.com>
2020-10-14 14:58:54 +0500

    integrated call pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'home#index'
-resources :users
+
+
+
+resource :users do
+  member do
+    get 'audio_call_1'
+    get 'audio_call_2'
+    get 'audio_calling_1'
+    get 'audio_calling_4'
+    get 'dashboard_schedule_appointment_success'
+    get 'dashboard_simple'
+    get 'dashboard'
+    get 'editprofile'
+    get 'message_chat_new'
+    get 'message_chat'
+    
+  end
+end
 end
